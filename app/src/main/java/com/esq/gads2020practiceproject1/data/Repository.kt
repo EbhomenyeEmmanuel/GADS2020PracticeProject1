dd23960ce6f239ea6ca84c200dc3a0138397369e
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-09-04 08:44:43 +0100

    Method added to get list from api.

--- app/src/main/java/com/esq/gads2020practiceproject1/data/Repository.kt
+++ app/src/main/java/com/esq/gads2020practiceproject1/data/Repository.kt
@@ -1,4 +1,11 @@
 package com.esq.gads2020practiceproject1.data
 
+import com.esq.gads2020practiceproject1.domain.network.RetrofitApi
+
 class Repository {
+
+    suspend fun getListOfLearningLeaders() = RetrofitApi.client.getLearningLeaders()
+
+    suspend fun getListOfSkillLeaders() = RetrofitApi.client.getSkillLeaders()
+
 }

2d3dfd6bf330872934229cb0e81be85d46d567e6
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-09-05 03:19:42 +0100

    Network observer library added.

--- app/src/main/java/com/esq/gads2020practiceproject1/MainActivity.kt
+++ app/src/main/java/com/esq/gads2020practiceproject1/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.esq.gads2020practiceproject1
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.viewpager.widget.ViewPager
+import com.biodun.networkMonitorManager.NetworkState
 import com.esq.gads2020practiceproject1.data.SectionsPagerAdapter
-import com.esq.gads2020practiceproject1.utils.longToast
+import com.esq.gads2020practiceproject1.utils.NetworkStatus
+import com.esq.gads2020practiceproject1.utils.shortToast
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     private var mToolbar: MaterialToolbar? = null
     private var mTabLayout: TabLayout? = null
     private var mViewPager: ViewPager? = null
+    private lateinit var networkStatus: NetworkStatus
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +32,31 @@ class MainActivity : AppCompatActivity() {
         mTabLayout = tabs
         mTabLayout!!.setupWithViewPager(viewPager)
         mTabLayout!!.setupWithViewPager(mViewPager)
+        networkStatus = NetworkStatus(this)
 
-        submitBtn.setOnClickListener {  }
+        submitBtn.setOnClickListener { }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        networkStatus.networkMonitorManager.registerCallback()
+        observeNetworkStateLiveData()
+    }
+
+    private fun observeNetworkStateLiveData() {
+        networkStatus.networkMonitorManager.networkState.observe(this, Observer { network ->
+            when (network) {
+                is NetworkState.Available -> {
+                    shortToast("Internet Available")
+                }
+                is NetworkState.UnAvailable -> shortToast("Internet Connection UnAvailable")
+                is NetworkState.Lost -> shortToast("Internet Connection Lost")
+            }
+        })
+    }
+
+    override fun onStop() {
+        super.onStop()
+        networkStatus.networkMonitorManager.unRegisterCallback()
     }
 }

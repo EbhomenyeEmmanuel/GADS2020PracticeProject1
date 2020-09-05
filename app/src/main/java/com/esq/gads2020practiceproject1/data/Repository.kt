b9b7d189944614e546ea368116d79e47feedaa12
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-09-05 03:20:29 +0100

    Correct data model were returned from the API

--- app/src/main/java/com/esq/gads2020practiceproject1/data/Repository.kt
+++ app/src/main/java/com/esq/gads2020practiceproject1/data/Repository.kt
@@ -1,11 +1,15 @@
 package com.esq.gads2020practiceproject1.data
 
+import com.esq.gads2020practiceproject1.domain.LeaderDataModel
+import com.esq.gads2020practiceproject1.domain.SkillDataModel
 import com.esq.gads2020practiceproject1.domain.network.RetrofitApi
 
 class Repository {
 
-    suspend fun getListOfLearningLeaders() = RetrofitApi.client.getLearningLeaders()
+    suspend fun getListOfLearningLeaders(): List<LeaderDataModel> =
+        RetrofitApi.client.getLearningLeaders()
 
-    suspend fun getListOfSkillLeaders() = RetrofitApi.client.getSkillLeaders()
+    suspend fun getListOfSkillLeaders(): List<SkillDataModel> =
+        RetrofitApi.client.getSkillLeaders()
 
 }

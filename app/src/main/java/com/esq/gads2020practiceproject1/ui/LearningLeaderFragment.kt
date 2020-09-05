04b8f8351465c501ffd09e9d3e48f0500699f17d
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-09-05 03:27:16 +0100

    Adapter, Recyclerview added and list observed.

--- app/src/main/java/com/esq/gads2020practiceproject1/ui/LearningLeaderFragment.kt
+++ app/src/main/java/com/esq/gads2020practiceproject1/ui/LearningLeaderFragment.kt
@@ -1,15 +1,25 @@
 package com.esq.gads2020practiceproject1.ui
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.esq.gads2020practiceproject1.R
+import com.esq.gads2020practiceproject1.data.LeaderAdapter
+import com.esq.gads2020practiceproject1.utils.Status
+import com.esq.gads2020practiceproject1.utils.longToast
+import com.esq.gads2020practiceproject1.utils.shortToast
 import com.esq.gads2020practiceproject1.viewmodel.LearningLeaderViewModel
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.learning_leader_fragment.*
 
 class LearningLeaderFragment : Fragment(R.layout.learning_leader_fragment) {
     val TAG = this::class.simpleName
+    lateinit var leaderAdapter: LeaderAdapter
 
     companion object {
         @JvmStatic
@@ -22,9 +32,38 @@ class LearningLeaderFragment : Fragment(R.layout.learning_leader_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(LearningLeaderViewModel::class.java)
-        viewModel.text.observe(viewLifecycleOwner, Observer<String> {
-            // textView.text = it
-        })
+        activity?.initLeaderAdapter()
     }
 
+    private fun Context.initLeaderAdapter() {
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        leaderRv!!.layoutManager = layoutManager
+        viewModel.learningLeaders.observe(viewLifecycleOwner, Observer {
+            it.let { resource ->
+                when (resource.status) {
+                    Status.LOADING -> {
+                        shortToast("Loading")
+                        Log.d(TAG, "Status.LOADING: Loading")
+                    }
+                    Status.SUCCESS -> {
+                        shortToast("Success")
+                        Log.d(TAG, "Status.SUCCESS: Success")
+                        Log.d(TAG, " Items observed is ${it.data}")
+                        leaderAdapter =
+                            LeaderAdapter(
+                                this,
+                                it.data!!
+                            )
+                        leaderRv.setAdapter(leaderAdapter)
+                    }
+                    Status.ERROR -> {
+                        longToast("Error!!!")
+                        Log.d(TAG, "Status.ERROR: Failed")
+                    }
+                }
+            }
+        })
+    }
 }
+
--- app/src/main/java/com/esq/gads2020practiceproject1/ui/SkillFragment.kt
+++ app/src/main/java/com/esq/gads2020practiceproject1/ui/SkillFragment.kt
@@ -1,15 +1,24 @@
 package com.esq.gads2020practiceproject1.ui
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.esq.gads2020practiceproject1.R
+import com.esq.gads2020practiceproject1.data.SkillAdapter
+import com.esq.gads2020practiceproject1.utils.Status
+import com.esq.gads2020practiceproject1.utils.longToast
+import com.esq.gads2020practiceproject1.utils.shortToast
 import com.esq.gads2020practiceproject1.viewmodel.SkillViewModel
+import kotlinx.android.synthetic.main.skill_fragment.*
 
 class SkillFragment : Fragment(R.layout.skill_fragment) {
     val TAG = this::class.simpleName
+lateinit var skillAdapter: SkillAdapter
 
     companion object {
         fun newInstance() = SkillFragment()
@@ -19,10 +28,37 @@ class SkillFragment : Fragment(R.layout.skill_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         viewModel = ViewModelProvider(this).get(SkillViewModel::class.java)
-        viewModel.text.observe(viewLifecycleOwner, Observer<String> {
-            //textView.text = it
-        })
-        super.onViewCreated(view, savedInstanceState)
+        activity?.initSkillAdapter()
     }
 
+    private fun Context.initSkillAdapter() {
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        skillRv!!.layoutManager = layoutManager
+        viewModel.skillLeaders.observe(viewLifecycleOwner, Observer {
+            it.let { resource ->
+                when (resource.status) {
+                    Status.LOADING -> {
+                        shortToast("Loading")
+                        Log.d(TAG, "Status.LOADING: Loading")
+                    }
+                    Status.SUCCESS -> {
+                        shortToast("Success")
+                        Log.d(TAG, "Status.SUCCESS: Success")
+                        Log.d(TAG, " Items observed is ${it.data}")
+                        skillAdapter =
+                            SkillAdapter(
+                                this,
+                                it.data!!
+                            )
+                       skillRv.setAdapter(skillAdapter)
+                    }
+                    Status.ERROR -> {
+                        longToast("Error!!!")
+                        Log.d(TAG, "Status.ERROR: Failed")
+                    }
+                }
+            }
+        })
+    }
 }

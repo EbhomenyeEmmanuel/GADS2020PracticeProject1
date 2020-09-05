8addd64de045f2d3d82a4e4c1ac96d7753178caa
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-09-05 03:35:32 +0100

    Changes made.

--- app/src/main/java/com/esq/gads2020practiceproject1/utils/NetworkStatus.kt
+++ app/src/main/java/com/esq/gads2020practiceproject1/utils/NetworkStatus.kt
@@ -1,17 +1,11 @@
 package com.esq.gads2020practiceproject1.utils
 
 import android.content.Context
-import android.os.Build
-import android.os.Bundle
-import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
 import com.biodun.networkMonitorManager.NetworkMonitorManager
 
- class NetworkStatus(context: Context) : LifecycleObserver {
+class NetworkStatus(context: Context) : LifecycleObserver {
     val TAG = this::class.simpleName
 
     init {
@@ -20,20 +14,6 @@ import com.biodun.networkMonitorManager.NetworkMonitorManager
 
     val networkMonitorManager: NetworkMonitorManager by lazy {
         NetworkMonitorManager.getInstance(context)
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    @OnLifecycleEvent(Lifecycle.Event.ON_START)
-    fun onStart() {
-        Log.d(TAG, "OnStart")
-        networkMonitorManager.registerCallback()
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
-    fun onStop() {
-        Log.d(TAG, "OnStop")
-        networkMonitorManager.unRegisterCallback()
     }
 
  }

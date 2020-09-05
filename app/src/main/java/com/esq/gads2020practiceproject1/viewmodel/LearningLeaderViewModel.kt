3c0f52a405d31e7cb88fe9990db8d53328d6e87d
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-09-05 03:22:40 +0100

    LiveData emitted from repo via a network bound resource class

--- app/src/main/java/com/esq/gads2020practiceproject1/viewmodel/LearningLeaderViewModel.kt
+++ app/src/main/java/com/esq/gads2020practiceproject1/viewmodel/LearningLeaderViewModel.kt
@@ -1,16 +1,23 @@
 package com.esq.gads2020practiceproject1.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.esq.gads2020practiceproject1.data.Repository
+import com.esq.gads2020practiceproject1.utils.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 class LearningLeaderViewModel : ViewModel() {
-    private val _index = MutableLiveData<Int>()
-    val text: LiveData<String> = Transformations.map(_index) {
-        "Hello world from section: $it"
-    }
+    private val repo by lazy { Repository() }
 
-    fun setIndex(index: Int) {
-        _index.value = index
-    }}
+    val learningLeaders = liveData {
+        viewModelScope.launch (Dispatchers.IO){
+            emit(Resource.loading(null))
+            try {
+                emit(Resource.success(repo.getListOfLearningLeaders()))
+            } catch (e: IOException) {
+                emit(Resource.error(e.message!!, data = null))
+            }
+        }
+    }
+}
--- app/src/main/java/com/esq/gads2020practiceproject1/viewmodel/SkillViewModel.kt
+++ app/src/main/java/com/esq/gads2020practiceproject1/viewmodel/SkillViewModel.kt
@@ -1,16 +1,28 @@
 package com.esq.gads2020practiceproject1.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import com.esq.gads2020practiceproject1.data.Repository
+import com.esq.gads2020practiceproject1.utils.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 class SkillViewModel : ViewModel() {
-    private val _index = MutableLiveData<Int>()
-    val text: LiveData<String> = Transformations.map(_index) {
-        "Hello world from section: $it"
+    private val repo by lazy { Repository() }
+
+    val skillLeaders = liveData {
+        viewModelScope.launch (Dispatchers.IO){
+            emit(Resource.loading(null))
+            try {
+                emit(Resource.success(repo.getListOfSkillLeaders()))
+            } catch (e: IOException) {
+                emit(Resource.error(e.message!!, data = null))
+            }
+        }
     }
 
-    fun setIndex(index: Int) {
-        _index.value = index
-    }}
+}
+
+

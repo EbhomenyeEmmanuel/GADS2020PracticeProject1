ae38fc9d5b4772050aa02ca50cc7178991731262
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-09-06 11:31:41 +0100

    Form annotation added.

--- app/src/main/java/com/esq/gads2020practiceproject1/domain/interfaces/SubmitApiInterface.kt
+++ app/src/main/java/com/esq/gads2020practiceproject1/domain/interfaces/SubmitApiInterface.kt
@@ -1,11 +1,13 @@
 package com.esq.gads2020practiceproject1.domain.interfaces
 
 import retrofit2.http.FieldMap
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface SubmitApiInterface {
 
     @POST("/1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse/")
+    @FormUrlEncoded
     suspend fun submitDetails(@FieldMap details: Map<String, String> )
 
 }

0a3ff795af8deccfe30d0608fd90df11e4d5ea10
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-09-12 00:45:11 +0100

    New method created.

--- app/src/main/java/com/esq/gads2020practiceproject1/domain/interfaces/SubmitDetailsCallback.kt
+++ app/src/main/java/com/esq/gads2020practiceproject1/domain/interfaces/SubmitDetailsCallback.kt
@@ -1,6 +1,7 @@
 package com.esq.gads2020practiceproject1.domain.interfaces
 
 interface SubmitDetailsCallback {
+    fun onValidationSuccess(successMessage: String)
     fun onValidationError(errorMessage: String, errorCode: Int)
     fun onSubmitSuccess(successMsg: String)
     fun onSubmitError(errorMessage: String)

9894748f9630b11d8017b35035f2d8b986f05d3f
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-09-12 04:06:18 +0100

    Changed parameter to string

--- app/src/main/java/com/esq/gads2020practiceproject1/domain/interfaces/SubmitDetailsCallback.kt
+++ app/src/main/java/com/esq/gads2020practiceproject1/domain/interfaces/SubmitDetailsCallback.kt
@@ -3,7 +3,7 @@ package com.esq.gads2020practiceproject1.domain.interfaces
 interface SubmitDetailsCallback {
     fun onValidationSuccess(successMessage: String)
     fun onValidationError(errorMessage: Int, errorCode: Int)
-    fun onSubmitSuccess(successMsg: Int)
-    fun onSubmitError(errorMessage: Int)
+    fun onSubmitSuccess(successMsg: String)
+    fun onSubmitError(errorMessage: String)
 
 }
